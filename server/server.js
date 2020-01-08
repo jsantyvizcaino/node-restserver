963c1c1d1ea2bd091a75630f1904e8e9d1ed5af1
jsantyvizcaino <jorge_santiagovizcaino@hotmail.com>
2020-01-08 13:09:19 -0500

    base de datos

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 require('./config/config')
 
 
+
+const mongoose = require('mongoose');
+
 //midleword capa intermedia entre el node js y el node js como tal  cada ves q un cliente hace peticiones pasa x aui  transfor l body de la pagina a un json
 // create application/x-www-form-urlencoded parser
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,43 +15,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.use(require('./routes/usuario'))
 
-app.get('/usuario', function(req, res) {
-    res.json("get Usuario");
+app.use(require('./routes/categorias'))
+
+app.use(require('./routes/producto'))
+
+
+
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+    if (err) throw err;
+
+    console.log("Base de datos ONLINE");
 });
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "el nombre es necesario"
-        });
-    } else {
-        res.json({
-            usuario: body
-        });
-    }
-
-
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.param.id
-    res.json({
-        id
-    })
-
-});
-
-
-app.delete('/usuario', function(req, res) {
-    res.json("delete Usuario");
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log("escuchando en el puerto", 3000);
