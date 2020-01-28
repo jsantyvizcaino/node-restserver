f3b3f08f48b6d3d27fb4aa291e2b6e172ca0d9cd
jsantyvizcaino <jorge_santiagovizcaino@hotmail.com>
2020-01-28 18:52:39 -0500

    final

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-beautiful-unique-validation');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE},no es un rol valido'
+}
 
 let Schema = mongoose.Schema; //conexion para mongo db
 
 let usuarioSchema = new Schema({
     nombre: {
         type: String,
-        required: ['true', 'el nombre es requerido']
+        required: [true, 'el nombre es requerido']
     },
     email: {
         type: String,
-        required: ['true', 'el correo es requerido']
+        required: [true, 'el correo es requerido'],
+        unique: true,
+        //disctint: true
     },
     password: {
         type: String,
-        required: ['true', 'el password es requerido']
+        required: [true, 'el password es requerido']
     },
     img: {
         type: String,
@@ -22,7 +30,8 @@ let usuarioSchema = new Schema({
     role: {
         type: String,
         default: "USER_ROLE",
-        // required: false
+        enum: rolesValidos
+            // required: false
 
     },
     estado: {
@@ -39,6 +48,17 @@ let usuarioSchema = new Schema({
     }
 
 });
+
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+//plugin agregando una funcionalidad al usuario schema
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' })
 
 
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,12 +1,46 @@
 const express = require('express');
 const Usuario = require('../models/usuario')
 const app = express();
+const bcript = require('bcryptjs');
+const _ = require('underscore')
 
 
 
 
 app.get('/usuario', function(req, res) {
-    res.json("get Usuario");
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    // Usuario.find({ google: true })
+    Usuario.find({}), 'nombre email role google'
+
+    .limit(limite)
+        .skip(desde)
+        .exec((err, usuario) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    cuantos: conteo,
+                    usuario,
+
+                })
+            })
+
+            // res.json({
+            //     ok: true,
+            //     usuario
+            // })
+
+        });
 });
 
 app.post('/usuario', function(req, res) {
@@ -15,8 +49,9 @@ app.post('/usuario', function(req, res) {
     let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
-        role: body.role
+        password: bcript.hashSync(body.password, 10),
+        role: body.role,
+        img: body.img
     });
 
     usuario.save((err, usuarioDB) => {
@@ -35,16 +70,58 @@ app.post('/usuario', function(req, res) {
 });
 
 app.put('/usuario/:id', function(req, res) {
-    let id = req.param.id
+    let id = req.params.id;
+    //let body = req.body;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado'])
+
+
+
+
+    // delete body.password;
+    // delete body.google
+
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+    })
+
+
+
     res.json({
-        id
+        cedula: id
     })
 
 });
 
 
-app.delete('/usuario', function(req, res) {
-    res.json("delete Usuario");
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    Usuario.findOneAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (usuarioBorrado === null) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            })
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        })
+    })
 });
 
 module.exports = app;
